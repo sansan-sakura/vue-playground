2d0642326e3fae300eb2e92330bb7829756ff3bb
Sakura Tanaka <sito6496t@gmail.com>
2024-02-09 22:46:55 +0100

    Add methods

--- app.js
+++ app.js
@@ -2,7 +2,17 @@ const app = Vue.createApp({
   data() {
     return {
       title: "Harry Potter",
+      age: 0,
+      showbooks: false,
     };
+  },
+  methods: {
+    changeTitle() {
+      this.title = "Narnia";
+    },
+    toggleBooks() {
+      this.showbooks = !this.showbooks;
+    },
   },
 });
 app.mount("#app");
